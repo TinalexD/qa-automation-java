f03d7705d4067241ac86ca4f03811c99d8df9697
a.v.demchenko <a.v.demchenko@tinkoff.ru>
2022-04-01 15:23:13 +0300

    feat: public variable messageCount added, prefix with count added to ConsolePrinter

--- src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
+++ src/main/java/com/tcs/edu/decorator/TimestampMessageDecorator.java
@@ -4,16 +4,20 @@ import java.time.Instant;
 
 /**
  * Класс для декорирования текста сообщений временем
+ *
  * @author a.v.demchenko
  */
 public class TimestampMessageDecorator {
+    public static int messageCount = 1;
+
     /**
      * Метод для декорирования сообщения временем в формате timestamp
+     *
      * @param message сообщение, которое необходимо задекорировать
      * @return переменная с задекорированным сообщением
      */
     public static String decorate(String message) {
-        String decoratedMessage = Instant.now() + " " + message;
+        String decoratedMessage = messageCount + " " + Instant.now() + " " + message;
         return decoratedMessage;
     }
 }
--- src/main/java/com/tcs/edu/printer/ConsolePrinter.java
+++ src/main/java/com/tcs/edu/printer/ConsolePrinter.java
@@ -1,15 +1,21 @@
 package com.tcs.edu.printer;
 
+import static com.tcs.edu.decorator.TimestampMessageDecorator.messageCount;
+
 /**
  * Класс предназначен для вывода сообщений на консоль
+ *
  * @author e.krivosheyev
  */
 public class ConsolePrinter {
+
     /**
      * Метод выводит переданное сообщение на консоль
+     *
      * @param message сообщение которое надо вывести на консоль
      */
     public static void print(String message) {
+        messageCount++;
         System.out.println(message);
     }
 }
