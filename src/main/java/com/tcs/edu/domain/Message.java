f74d2580a75c1d019054c3d126e1366508c4b6ed
a.v.demchenko <a.v.demchenko@tinkoff.ru>
2022-05-16 22:21:58 +0300

    Feat: Добавил методы equals и toString в класс Message. Добавил к тест сравнение объектов.

--- src/main/java/com/tcs/edu/domain/Message.java
+++ src/main/java/com/tcs/edu/domain/Message.java
@@ -2,6 +2,8 @@ package com.tcs.edu.domain;
 
 import com.tcs.edu.decorator.Severity;
 
+import java.util.Objects;
+
 public class Message {
     private String message;
     private Severity level;
@@ -24,6 +26,28 @@ public class Message {
 
     public String getMessage() {
         return message;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "message='" + message + '\'' +
+                ", level=" + level +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message1 = (Message) o;
+        return Objects.equals(getMessage(), message1.getMessage()) &&
+                getLevel() == message1.getLevel();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getMessage(), getLevel());
     }
 
     public Severity getLevel() {
