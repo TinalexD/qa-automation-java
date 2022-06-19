d8b9ce4f304d4b6a34c09d2e0877d1bc3455c137
a.v.demchenko <a.v.demchenko@tinkoff.ru>
2022-06-19 23:52:37 +0300

    Feat: Добавил добавил коннекты, покрыл базовые сценарии с добавлением кейсов для независимого тестирования. Добавил зависимости обновил докер-файл.

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -1,7 +1,17 @@
 import io.restassured.RestAssured;
 import io.restassured.authentication.PreemptiveBasicAuthScheme;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
@@ -11,13 +21,25 @@ import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.hamcrest.Matchers.not;
 
 public class ApiTest {
+    private static Connection connection;
+    private static int testCountryID;
+
 
     @BeforeAll
-    public static void getAuth() {
+    public static void testsPreparations() throws SQLException {
+        connection = DriverManager.getConnection(
+                "jdbc:postgresql://localhost/app-db",
+                "app-db-admin",
+                "P@ssw0rd"
+        );
+        Statement alterTable = connection.createStatement();
+        alterTable.execute("alter table country ALTER COLUMN id ADD generated always as identity (START WITH 11)");
+
         PreemptiveBasicAuthScheme authScheme = new PreemptiveBasicAuthScheme();
         authScheme.setUserName("admin");
         authScheme.setPassword("admin");
         RestAssured.authentication = authScheme;
+
     }
 
     @BeforeAll
@@ -25,12 +47,41 @@ public class ApiTest {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
     }
 
+    @BeforeEach
+    public void createTestsData() throws SQLException {
+        PreparedStatement sql = connection.prepareStatement(
+                "INSERT INTO country(country_name) VALUES(?)",
+                Statement.RETURN_GENERATED_KEYS);
+        sql.setString(1, "TO");
+        sql.executeUpdate();
+        ResultSet result = sql.getGeneratedKeys();
+        result.next();
+        testCountryID = result.getInt(1);
+    }
+
+    @AfterEach
+    public void clearTestsData() throws SQLException {
+        Statement check = connection.createStatement();
+        ResultSet result = check.executeQuery("SELECT * FROM country where id > 10");
+        if (result.next()){
+            PreparedStatement delete = connection.prepareStatement("DELETE FROM country WHERE ID >10");
+            delete.executeUpdate();
+        };
+    }
+
+    @AfterAll
+    public static void disconnect() throws SQLException {
+        Statement alterTable = connection.createStatement();
+        alterTable.execute("alter table country ALTER COLUMN id DROP IDENTITY");
+        connection.close();
+    }
+
     @Test
     public void createCountry() {
         given()
                 .contentType("application/json")
                 .body("{\n" +
-                        "  \"countryName\": \"RU\"\n" +
+                        "  \"countryName\": \"II\"\n" +
                         "}")
         .when()
                 .post("api/countries")
@@ -38,14 +89,15 @@ public class ApiTest {
                 .statusCode(201)
                 .body("id", not(empty()));
 
+
     }
 
     @Test
     public void findCountry() {
         when()
-                .get("api/countries?countryName.contains=st").
+                .get("api/countries?countryName.contains=TO").
         then()
-                .statusCode(200).body("[0].id", is(3));
+                .statusCode(200).body("[0].id", is(testCountryID));
     }
 
     @Test
@@ -53,24 +105,24 @@ public class ApiTest {
         given()
                 .contentType("application/json")
                 .body("{\n" +
-                        "  \"id\": 1,\n" +
-                        "  \"countryName\": \"nl\"\n" +
+                        "  \"id\": " +testCountryID + ",\n" +
+                        "  \"countryName\": \"TT\"\n" +
                         "}")
         .when()
-                .patch("api/countries/1")
+                .patch("api/countries/" + testCountryID)
         .then()
                 .statusCode(200)
-                .body("id", is(1),
-                        "countryName", is("nl"));
+                .body("id", is(testCountryID),
+                        "countryName", is("TT"));
     }
 
     @Test
     public void deleteCountry() {
         when()
-                .delete("api/countries/2").then().statusCode(204);
+                .delete("api/countries/" + testCountryID).then().statusCode(204);
 
         when()
-                .get("api/countries?id.equals=2").
+                .get("api/countries?id.equals=" + testCountryID).
         then()
                 .statusCode(200).body("[0]", isEmptyOrNullString());
     }
